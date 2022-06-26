fed2d9297e499837f4b8b549ffe4827914bc045e
Deon Butler <deonbutler44@gmail.com>
2022-06-26 15:12:22 -0400

    Solution now stores data in a collection rather then just printing.

--- BeautifulSoupIntro.py
+++ BeautifulSoupIntro.py
@@ -6,9 +6,20 @@ res = requests.get(MARA)
 resText = res.text
 soup = BeautifulSoup(resText, 'html.parser')
 
-tables = soup.find_all('table')
 trs = soup.find_all('tr')
 importantData = trs[:16]
 
 for x in range(len(importantData)):
     print(trs[x].span.text + ':', trs[x].contents[1].text)
+
+
+wikiLink = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+response = requests.get(wikiLink)
+responseText = response.text
+soup2 = BeautifulSoup(responseText, 'html.parser')
+
+trs2 = soup2.find_all('tr')
+stockTickers = trs2[:504]
+
+for x in range(len(stockTickers)):
+    print(trs2[x].a.text)
