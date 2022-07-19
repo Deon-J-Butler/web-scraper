fe8ec10aa6e1eb459971486e9d46297ab829958e
DeonB <DeonButler44@gmail.com>
2022-07-19 10:53:25 -0400

    implicit wait timer to find data and close immediately after data pull, headless driver to keep GUI from opening.

--- dynamic-data-pull.py
+++ dynamic-data-pull.py
@@ -1,32 +1,39 @@
 import requests, time
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.edge.service import Service as EdgeService
-from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
 
 url = "https://finance.yahoo.com/chart/AAL#eyJpbnRlcnZhbCI6ImRheSIsInBlcmlvZGljaXR5IjoxLCJ0aW1lVW5pdCI6bnVsbCwiY2FuZGxlV2lkdGgiOjgsImZsaXBwZWQiOmZhbHNlLCJ2b2x1bWVVbmRlcmxheSI6dHJ1ZSwiYWRqIjp0cnVlLCJjcm9zc2hhaXIiOnRydWUsImNoYXJ0VHlwZSI6ImxpbmUiLCJleHRlbmRlZCI6ZmFsc2UsIm1hcmtldFNlc3Npb25zIjp7fSwiYWdncmVnYXRpb25UeXBlIjoib2hsYyIsImNoYXJ0U2NhbGUiOiJsaW5lYXIiLCJzdHVkaWVzIjp7IuKAjHZvbCB1bmRy4oCMIjp7InR5cGUiOiJ2b2wgdW5kciIsImlucHV0cyI6eyJpZCI6IuKAjHZvbCB1bmRy4oCMIiwiZGlzcGxheSI6IuKAjHZvbCB1bmRy4oCMIn0sIm91dHB1dHMiOnsiVXAgVm9sdW1lIjoiIzAwYjA2MSIsIkRvd24gVm9sdW1lIjoiI2ZmMzMzYSJ9LCJwYW5lbCI6ImNoYXJ0IiwicGFyYW1ldGVycyI6eyJ3aWR0aEZhY3RvciI6MC40NSwiY2hhcnROYW1lIjoiY2hhcnQifX19LCJwYW5lbHMiOnsiY2hhcnQiOnsicGVyY2VudCI6MSwiZGlzcGxheSI6IkFBTCIsImNoYXJ0TmFtZSI6ImNoYXJ0IiwiaW5kZXgiOjAsInlBeGlzIjp7Im5hbWUiOiJjaGFydCIsInBvc2l0aW9uIjpudWxsfSwieWF4aXNMSFMiOltdLCJ5YXhpc1JIUyI6WyJjaGFydCIsIuKAjHZvbCB1bmRy4oCMIl19fSwic2V0U3BhbiI6e30sImxpbmVXaWR0aCI6Miwic3RyaXBlZEJhY2tncm91bmQiOnRydWUsImV2ZW50cyI6dHJ1ZSwiY29sb3IiOiIjMDA4MWYyIiwic3RyaXBlZEJhY2tncm91ZCI6dHJ1ZSwiZXZlbnRNYXAiOnsiY29ycG9yYXRlIjp7ImRpdnMiOnRydWUsInNwbGl0cyI6dHJ1ZX0sInNpZ0RldiI6e319LCJzeW1ib2xzIjpbeyJzeW1ib2wiOiJBQUwiLCJzeW1ib2xPYmplY3QiOnsic3ltYm9sIjoiQUFMIiwicXVvdGVUeXBlIjoiRVFVSVRZIiwiZXhjaGFuZ2VUaW1lWm9uZSI6IkFtZXJpY2EvTmV3X1lvcmsifSwicGVyaW9kaWNpdHkiOjEsImludGVydmFsIjoiZGF5IiwidGltZVVuaXQiOm51bGwsInNldFNwYW4iOnt9fV19"
 
-req = requests.get(url)
-text = req.text
-
+fOptions = webdriver.FirefoxOptions()
+fOptions.add_argument("headless")
 firefoxDriver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+cOptions = webdriver.ChromeOptions()
+cOptions.add_argument("headless")
 chromeDriver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-edgeDriver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 
 #Choose your correct driver
-edgeDriver.get(url)
+firefoxDriver.get(url)
 
-#Set sleep timer so data can adequately pull before web page closes
-time.sleep(5)
+#Setting the max wait time for page/contents to load
+wait = WebDriverWait(firefoxDriver, 5)
+lastLoadedElementXPath = '//*[@id="data-util-col"]/section[2]/table/tbody/tr[1]/td/p'
+xPath = '//*[@id="data-util-col"]/section[2]/table/tbody/tr[1]'
 
-for i in range(1, 6):
-    #Can be retreived by using browser devtools, selecting element in question, and choosing "Copy XPath"
-    xPath = '//*[@id="data-util-col"]/section[2]/table/tbody/tr['+str(i)+']'
-    elem = edgeDriver.find_elements_by_xpath(xPath)
+try:
+    wait.until(ec.visibility_of_element_located(By.XPATH, lastLoadedElementXPath))
+    elem = firefoxDriver.find_elements_by_xpath(xPath)
     print(elem[0].text.split("\n"))
+except Exception as e:
+    print(str(e))
+    print("Element not visible.")
 
 #Close your driver
-edgeDriver.close()
+firefoxDriver.close()
